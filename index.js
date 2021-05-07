3b8317bf5a8279be39d53408e9052fc6cb3870ec
Jacek Kościesza <jacek.kosciesza@gmail.com>
2021-05-07 16:34:19 +0200

    *.cjs

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 #! /usr/bin/env node
 
-import * as path from "path";
-
 const nodePlop = require('node-plop');
+const path = require('path');
 
-const plopfileJsPath = path.join(__dirname, "plopfile.js");
+const plopfileJsPath = path.join(__dirname, "/plopfile.js");
 console.log(plopfileJsPath);
 const plop = nodePlop(plopfileJsPath);
 const generator = plop.getGenerator('controller');
